e6e24b8ebf98c073d7d01cc8fc88dcc2bd6f890b
nikeub1 <nikeuprof@gmail.com>
2025-04-25 02:28:53 +0400

    Nikoloz's Work
    for calculation history

---
+++ Calculator/Services/CalculatorService.cs
@@ -0,0 +1,36 @@
+﻿using Calculator.Web.Models;
+using System;
+using System.Data;
+
+namespace Calculator.Services
+{
+    public class CalculatorService
+    {
+        private readonly ICalculationHistoryService _history;
+
+        public CalculatorService(ICalculationHistoryService history)
+        {
+            _history = history;
+        }
+
+        public double Calculate(string expression)
+        {
+            var result = Evaluate(expression);
+            _history.Add(new CalculationRecord
+            {
+                Expression = expression,
+                Result = result,
+                Timestamp = DateTime.Now
+            });
+            return result;
+        }
+
+        private double Evaluate(string expression)
+        {
+            var table = new DataTable();
+            return Convert.ToDouble(table.Compute(expression, string.Empty));
+        }
+
+        public IEnumerable<CalculationRecord> GetHistory() => _history.GetAll();
+    }
+}
